e90a8f4b9680ee55407a27fcb6eab7d27e649340
vladi99 <vlpetrovic99@gmail.com>
2017-09-01 19:34:25 +0300

    add colour balls problem

---
+++ DSA/DSA-ExamPreparation/ColourBalls/ColourBalls.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace ColourBalls
+{
+    class ColourBalls
+    {
+        static void Main(string[] args)
+        {
+            var factorials = new BigInteger[31];
+            factorials[0] = 1;
+            for (int i = 1; i < factorials.Length; i++)
+            {
+                factorials[i] = factorials[i - 1] * i;
+            }
+            string input = Console.ReadLine();
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (dict.ContainsKey(input[i]))
+                {
+                    dict[input[i]]++;
+                }
+                else
+                {
+                    dict[input[i]] = 1;
+                }
+            }
+
+            BigInteger result = factorials[input.Length];
+            foreach (var value in dict.Values)
+            {
+                result /= factorials[value];
+            }
+            Console.WriteLine(result);
+        }
+        
+    }
+}
--- DSA/DSA-ExamPreparation/ColourBalls/Properties/AssemblyInfo.cs
+++ DSA/DSA-ExamPreparation/ColourBalls/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bridges")]
+[assembly: AssemblyTitle("ColourBalls")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bridges")]
+[assembly: AssemblyProduct("ColourBalls")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8dffa6ab-71f8-4e16-99c8-d59c5de46733")]
+[assembly: Guid("0e79395c-fcdf-4d6e-80ff-f988bc3a612e")]
 
 // Version information for an assembly consists of the following four values:
 //
