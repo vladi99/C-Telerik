9e10eefc5f312ee88361a88f2abc9098179c8620
vladi99 <vlpetrovic99@gmail.com>
2017-09-01 19:32:32 +0300

    Businessmen problem

---
+++ DSA/DSA-ExamPreparation/Businessmen/Businessmen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace Businessmen
+{
+    class Businessmen
+    {
+        static void Main(string[] args)
+        {
+            var factorials = new BigInteger[141];
+            factorials[0] = 1;
+            for (int i = 1; i < factorials.Length; i++)
+            {
+                factorials[i] = factorials[i - 1] * i;
+            }
+            int n = int.Parse(Console.ReadLine());
+            BigInteger result = factorials[n] / (factorials[n/2] * factorials[n/2+1]);
+            Console.WriteLine(result);
+        }
+    }
+}
