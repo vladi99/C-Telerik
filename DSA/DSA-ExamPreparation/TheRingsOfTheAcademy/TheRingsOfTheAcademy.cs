5efa161f8fb6d6bd7dcebc8292c200e0f93d39c8
vladi99 <vlpetrovic99@gmail.com>
2017-09-02 14:36:55 +0300

    add the rings of the academy problem

---
+++ DSA/DSA-ExamPreparation/TheRingsOfTheAcademy/TheRingsOfTheAcademy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+namespace TheRingsOfTheAcademy
+{
+    class TheRingsOfTheAcademy
+    {
+        static void Main(string[] args)
+        {
+            var factorials = new BigInteger[22];
+            factorials[0] = 1;
+            for (int i = 1; i < factorials.Length; i++)
+            {
+                factorials[i] = factorials[i - 1] * i;
+            }
+            int[] arr = new int[22];
+            BigInteger result = 1;
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int index = int.Parse(Console.ReadLine());
+                arr[index]++;
+            }
+
+            for (int i = 0; i < 22; i++)
+            {
+                if (arr[i] > 1)
+                {
+                    result *= factorials[arr[i]];
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
