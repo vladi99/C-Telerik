7e5dd150d58768b5e484078c12d864f620a14a77
vladi99 <vlpetrovic99@gmail.com>
2017-09-02 13:58:20 +0300

    add gold fever prolem

---
+++ DSA/DSA-ExamPreparation/GoldFever/GoldFever.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace GoldFever
+{
+    class GoldFever
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] goldPrice = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            long result = 0;
+            for (int i = n - 1; i > 0; i--)
+            {
+                if (goldPrice[i] > goldPrice[i - 1])
+                {
+                    result += goldPrice[i] - goldPrice[i - 1];
+                    goldPrice[i - 1] = goldPrice[i];
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
