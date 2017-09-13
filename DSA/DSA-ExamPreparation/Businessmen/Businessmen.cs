17e81b5f5e22a8de49556f0fa586f755c5136dd6
vladi99 <vlpetrovic99@gmail.com>
2017-09-13 23:41:06 +0300

    add design pattern example

--- DSA/DSA-ExamPreparation/Businessmen/Businessmen.cs
+++ DSA/DSA-ExamPreparation/Businessmen/Businessmen.cs
@@ -7,14 +7,14 @@ namespace Businessmen
     {
         static void Main(string[] args)
         {
-            var factorials = new BigInteger[141];
+            var factorials = new BigInteger[2001];
             factorials[0] = 1;
             for (int i = 1; i < factorials.Length; i++)
             {
                 factorials[i] = factorials[i - 1] * i;
             }
             int n = int.Parse(Console.ReadLine());
-            BigInteger result = factorials[n] / (factorials[n/2] * factorials[n/2+1]);
+            BigInteger result = factorials[2*n] / (factorials[n] * factorials[n+1]);
             Console.WriteLine(result);
         }
     }
